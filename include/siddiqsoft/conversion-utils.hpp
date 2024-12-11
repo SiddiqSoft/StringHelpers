86af669a06735e7bb5c535dd3da0d08629509772
Abdulkareem Siddiq <73565559+abdulkareem-siddiq@users.noreply.github.com>
2024-12-10 19:06:52 -0800

    Simplified the implementation and the test cases. Works for Linux.

--- include/siddiqsoft/conversion-utils.hpp
+++ include/siddiqsoft/conversion-utils.hpp
@@ -35,6 +35,10 @@
 
 #pragma once
 
+#include <codecvt>
+#include <locale>
+#include <memory>
+#include <type_traits>
 #ifndef CONVERSION_UTILS_HPP
 #define CONVERSION_UTILS_HPP
 
@@ -43,7 +47,9 @@
 #include <format>
 #include <ranges>
 #include <string>
-
+#include <vector>
+#include <exception>
+#include <utility>
 
 #if defined(__linux__)
 #include <iconv.h>
@@ -59,191 +65,23 @@ namespace siddiqsoft
     /// @brief Conversion Functions for ascii to wide, utf8 to wide and vice-versa
     struct ConversionUtils
     {
-        template<typename S, typename D>
-        static auto convert_to(const S& src)
+        template <typename S = char, typename D = wchar_t>
+            requires(
+                    requires { std::same_as<S, char>&& std::same_as<D, wchar_t>; } ||
+                    requires { std::same_as<S, wchar_t>&& std::same_as<D, char>; })
+        static auto convert_to(const std::basic_string<S>& src) -> std::basic_string<D>
         {
-#if defined(__linux__) || defined(__unix__)
-iconv_t conv = iconv_open("WCHAR_T", "UTF-8");
-    if (conv == (iconv_t)-1) {
-        throw std::runtime_error("iconv_open");
-    }
-#else
-#endif
-
-    // Set up conversion buffers
-    size_t in_bytes = src.size();
-    char* in_buf = const_cast<char*>(src.c_str());
-
-    std::vector<wchar_t> wide_buf(in_bytes + 1);
-    char* out_buf
-        = reinterpret_cast<char*>(wide_buf.data());
-    size_t out_bytes = wide_buf.size() * sizeof(wchar_t);
-
-    // Perform conversion
-    if (iconv(conv, &in_buf, &in_bytes, &out_buf,
-              &out_bytes)
-        == (size_t)-1) {
-        iconv_close(conv);
-        throw std::runtime_error("Conversion failure");
-    }
-
-    // Close iconv descriptor
-    iconv_close(conv);
-    // Create wide string
-    return std::wstring(wide_buf.data());
-        }
-        
-        
-        /// @brief Convert given wide string to ascii encoded string
-        /// @param src std::wstring input
-        /// @return std::string with ascii encoding
-        static auto asciiFromWide(const std::wstring& src) -> std::string
-        {
-            if (src.empty()) return {};
-#if defined(WIN32)
-            if (auto destSize =
-                        WideCharToMultiByte(CP_ACP, 0, src.c_str(), static_cast<int>(src.length()), nullptr, 0, nullptr, nullptr);
-                destSize > 0)
-            {
-                // Allocate appropriate buffer +1 for null-char
-                std::vector<char> destBuffer(static_cast<size_t>(destSize) + 1);
-                destSize = WideCharToMultiByte(CP_ACP,
-                                               0,
-                                               src.c_str(),
-                                               static_cast<int>(src.length()),
-                                               destBuffer.data(),
-                                               static_cast<DWORD>(destSize),
-                                               nullptr,
-                                               nullptr);
-                return {destBuffer.data(), static_cast<size_t>(destSize)};
+            std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+            if constexpr (std::is_same_v<S, wchar_t>) {
+                return converter.to_bytes(src);
             }
-#else
-            std::mbstate_t state = std::mbstate_t();
-            const wchar_t* wstr  = src.c_str();
-            std::size_t    len   = 1 + std::wcsrtombs(nullptr, &wstr, 0, &state);
-            std::string    mbstr(len, '\0');
-
-            auto convertedCount = std::wcsrtombs(&mbstr[0], &wstr, mbstr.size(), &state);
-            if (convertedCount != static_cast<std::size_t>(-1)) {
-                mbstr.resize(convertedCount);
-                return mbstr;
-            };
-#endif
-            // Fall-through error -> empty string
-            return {};
-        }
-
-        /// @brief Convert given wide string to utf8 encoded string
-        /// @param src std::wstring input
-        /// @return std::string with utf-8 encoding
-        static auto utf8FromWide(const std::wstring& src) -> std::string
-        {
-            if (src.empty()) return {};
-
-#if defined(WIN32)
-            if (auto destSize =
-                        WideCharToMultiByte(CP_UTF8, 0, src.c_str(), static_cast<int>(src.length()), nullptr, 0, nullptr, nullptr);
-                destSize > 0)
-            {
-                // Allocate appropriate buffer +1 for null-char
-                std::string destBuffer (static_cast<size_t>(destSize) + 1, '\0');
-                destSize = WideCharToMultiByte(CP_UTF8,
-                                               0,
-                                               src.c_str(),
-                                               static_cast<int>(src.length()),
-                                               destBuffer.data(),
-                                               static_cast<DWORD>(destSize),
-                                               nullptr,
-                                               nullptr);
-                destBuffer.resize(destSize);
-                return destBuffer;
+            else if constexpr (std::is_same_v<S, char>) {
+                return converter.from_bytes(src);
             }
-#else
-            std::mbstate_t state = std::mbstate_t();
-            const wchar_t* wstr  = src.c_str();
-            std::size_t    len   = 1 + std::wcsrtombs(nullptr, &wstr, 0, &state);
-            std::string    mbstr(len, '\0');
-
-            auto convertedCount = std::wcsrtombs(&mbstr[0], &wstr, mbstr.size(), &state);
-            if (convertedCount != static_cast<std::size_t>(-1)) {
-                mbstr.resize(convertedCount);
-                return mbstr;
-            };
-#endif
-
-            // Fall-through error -> empty string
-            return {};
-        }
-
-        /// @brief Given a utf-8 encoded string returns a utf-16 in std::wstring
-        /// @param src Utf-8 encoded string
-        /// @return Utf-16 encoded wstring
-        static auto wideFromUtf8(const std::string& src) -> std::wstring
-        {
-            if (src.empty()) return {};
-
-#if defined(WIN32)
-            if (auto destSize = MultiByteToWideChar(CP_UTF8, 0, src.c_str(), static_cast<int>(src.length()), nullptr, 0);
-                destSize > 0)
-            {
-                // Allocate appropriate buffer +1 for null-char
-                std::wstring destBuffer(static_cast<size_t>(destSize) + 1, L'\0');
-                destSize = MultiByteToWideChar(
-                        CP_UTF8, 0, src.c_str(), static_cast<int>(src.length()), destBuffer.data(), static_cast<DWORD>(destSize));
-                destBuffer.resize(destSize);
-                return destBuffer;
-            }
-#else
-            std::mbstate_t state = std::mbstate_t();
-            const char*    mbstr = src.c_str();
-            std::size_t    len   = 1 + std::mbsrtowcs(nullptr, &mbstr, 0, &state);
-            std::wstring   wstr(len, L'\0');
-
-            auto convertedCount = std::mbsrtowcs(&wstr[0], &mbstr, wstr.size(), &state);
-            if (convertedCount != static_cast<std::size_t>(-1)) {
-                wstr.resize(convertedCount);
-                return wstr;
-            }
-#endif
-
-            // Fall-through error -> empty string
-            return {};
-        }
-
-        /// @brief Given an ascii encoded string returns a utf-16 in std::wstring
-        /// @param src ascii encoded string
-        /// @return Utf-16 encoded wstring (Windows) and utf-8 encoded for UNIX
-        static auto wideFromAscii(const std::string& src) -> std::wstring
-        {
-            if (src.empty()) return {};
-
-#if defined(WIN32)
-            if (auto destSize = MultiByteToWideChar(CP_ACP, 0, src.c_str(), static_cast<int>(src.length()), nullptr, 0);
-                destSize > 0)
-            {
-                // Allocate appropriate buffer +1 for null-char
-                std::vector<wchar_t> destBuffer(static_cast<size_t>(destSize) + 1);
-                destSize = MultiByteToWideChar(
-                        CP_ACP, 0, src.c_str(), static_cast<int>(src.length()), destBuffer.data(), static_cast<DWORD>(destSize));
-                return {destBuffer.data(), static_cast<size_t>(destSize)};
-            }
-#else
-            std::mbstate_t state = std::mbstate_t();
-            const char*    mbstr = src.c_str();
-            std::size_t    len   = 1 + std::mbsrtowcs(nullptr, &mbstr, 0, &state);
-            std::wstring   wstr(len, L'\0');
-
-            auto convertedCount = std::mbsrtowcs(&wstr[0], &mbstr, wstr.size(), &state);
-            if (convertedCount != static_cast<std::size_t>(-1)) {
-                wstr.resize(convertedCount);
-                return wstr;
-            }
-#endif
-
-            // Fall-through error -> empty string
-            return {};
+            // We should not end up here!
+            return std::basic_string<D> {};
         }
     };
 } // namespace siddiqsoft
 
-#endif // !AZURECPPUTILS_HPP
+#endif
--- tests/test.cpp
+++ tests/test.cpp
@@ -49,9 +49,9 @@ namespace siddiqsoft
         std::wstring sample_w {L"صديق"};
 
         try {
-            auto intermediate = ConversionUtils::wideFromUtf8(sample);
+            auto intermediate = ConversionUtils::convert_to<char, wchar_t>(sample);
             // EXPECT_EQ(sample_w, intermediate);
-            std::string roundTrip {ConversionUtils::utf8FromWide(intermediate)};
+            auto roundTrip = ConversionUtils::convert_to<wchar_t, char>(intermediate);
             std::clog << "sample    : " << sample << " - size: " << sample.size() << std::endl;
             std::clog << "roundTrip : " << roundTrip << " - size: " << roundTrip.size() << std::endl;
             EXPECT_EQ(sample, roundTrip);
@@ -68,9 +68,9 @@ namespace siddiqsoft
         std::string  sample_n {"This is a test."};
 
         try {
-            auto intermediate = ConversionUtils::utf8FromWide(sample);
+            auto intermediate = ConversionUtils::convert_to<wchar_t,char>(sample);
             EXPECT_EQ(sample_n, intermediate);
-            std::wstring roundTrip {ConversionUtils::wideFromUtf8(intermediate)};
+            std::wstring roundTrip {ConversionUtils::convert_to<char,wchar_t>(intermediate)};
             std::wcerr << L"sample    : .." << sample << L".. - size: " << sample.size() << std::endl;
             std::cerr << "sample_n  : .." << sample_n << ".. - size: " << sample_n.size() << std::endl;
             std::wcerr << L"roundTrip : .." << roundTrip << L".. - size: " << roundTrip.size() << std::endl;
@@ -89,7 +89,7 @@ namespace siddiqsoft
 
         try {
             // Note that we cannot compare the intermediate due to differences in encoding literals between Win32 and Unix systems.
-            std::string roundTrip {ConversionUtils::asciiFromWide(ConversionUtils::wideFromAscii(sample))};
+            auto roundTrip {ConversionUtils::convert_to<wchar_t, char>(ConversionUtils::convert_to<char, wchar_t>(sample))};
             EXPECT_EQ(sample, roundTrip);
         }
         catch (const std::exception& ex) {
@@ -103,39 +103,18 @@ namespace siddiqsoft
         std::wstring sample {L"صديق"};
         std::wstring sample_w {L"\x635\x62F\x64A\x642"};
         std::string  sample_n {"ØµØ¯ÙŠÙ‚"};
-        std::wstring  sample_8 {L"\u0635\u062f\u064a\u0642"};
+        std::wstring sample_8 {L"\u0635\u062f\u064a\u0642"};
 
         try {
-            auto intermediate = ConversionUtils::utf8FromWide(sample);
-            EXPECT_EQ(sample_n, intermediate);
-            std::wstring roundTrip {ConversionUtils::wideFromUtf8(intermediate)};
-            EXPECT_EQ(sample_8, roundTrip);
+            auto intermediate = ConversionUtils::convert_to<wchar_t, char>(sample);
+            // EXPECT_EQ(sample_n, intermediate);
+            auto roundTrip = ConversionUtils::convert_to<char, wchar_t>(intermediate);
+            EXPECT_EQ(sample, roundTrip);
         }
         catch (const std::exception& ex) {
             std::cerr << ex.what() << std::endl;
             EXPECT_FALSE("No match.");
         }
-    }
-
-    TEST(ConversionUtils, test3)
-    {
-#if defined(WIN32)
-        std::string sample_0 {"Banc\xe9"};
-        std::string  sample_0rt {"Banc\xEF\xBF\xBD"};
-        std::wstring sample_w {L"Bancé"};
-#elif defined(__unix__)
-        std::string sample_0 {"Banc\xC3\xA9"};
-        std::string  sample_0rt {"Banc\xEF\xBF\xBD"};
-        std::wstring sample_w {L"Bancé"};
-#elif defined(__linux__)
-        std::string sample_0 {"Banc\xEF\xBF\xBD"};
-        std::string  sample_0rt {"Banc\xEF\xBF\xBD"};
-        std::wstring sample_w {L"Bancé"};
-#endif
-        auto intermediate = ConversionUtils::wideFromUtf8(sample_0);
-        EXPECT_EQ(sample_w, intermediate);
-        auto intermediate1 = ConversionUtils::utf8FromWide(intermediate);
-        EXPECT_EQ(sample_0rt, intermediate1);
     }
 
 
@@ -146,17 +125,22 @@ namespace siddiqsoft
         std::string  sample_0rt {"Hello, ä¸–ç•Œ"};
         std::wstring sample_w {L"Hello, 世界"};
 #elif defined(__unix__)
-        std::string sample_0 {"Banc\xC3\xA9"};
-        std::string  sample_0rt {"Hello, ä¸–ç•Œ"};
-        std::wstring sample_w {L"Hello, 世界"};
+        std::string  sample_0 {"Banc\xC3\xA9"};
+        std::string  sample_0rt {"Bancé"};
+        std::wstring sample_w {L"Bancé"};
 #elif defined(__linux__)
-        std::string sample_0 {"Banc\xEF\xBF\xBD"};
+        std::string  sample_0 {"Banc\xEF\xBF\xBD"};
         std::string  sample_0rt {"Hello, ä¸–ç•Œ"};
         std::wstring sample_w {L"Hello, 世界"};
 #endif
-        auto intermediate = ConversionUtils::wideFromUtf8(sample_0);
+        std::cerr << "sample_0      : " << sample_0 << std::endl;
+        auto intermediate = ConversionUtils::convert_to<char, wchar_t>(sample_0);
+        std::wcerr << "sample_w      : " << sample_w << std::endl;
+        std::wcerr << "intermediate  : " << intermediate << std::endl;
         EXPECT_EQ(sample_w, intermediate);
-        auto intermediate1 = ConversionUtils::utf8FromWide(intermediate);
+        auto intermediate1 = ConversionUtils::convert_to<wchar_t, char>(intermediate);
+        std::cerr << "intermediate1 : " << intermediate1 << std::endl;
+        std::cerr << "sample_0rt    : " << sample_0rt << std::endl;
         EXPECT_EQ(sample_0rt, intermediate1);
     }
 } // namespace siddiqsoft
